9c70980eb9d389be4787d87cf06f7b465a2470f3
Sergey Klevakin <justerest@yandex.ru>
2017-12-23 00:43:25 +0500

    Release 0.3.0

--- main.js
+++ main.js
@@ -1,52 +1,57 @@
-require('./lib/stdio');
+const stdio = require('./lib/stdio');
 
 module.exports = async function main() {
-  writeln('ax^2 + bx + c = 0');
+  stdio.writeln('ax^2 + bx + c = 0');
 
-  const a = await readln({
+  const a = await stdio.readln({
     message: 'a = ',
     type: 'number',
     default: 1,
   });
 
-  const b = await readln({
+  const b = await stdio.readln({
     message: 'b = ',
     type: 'number',
     default: -4,
   });
 
-  const c = await readln({
+  const c = await stdio.readln({
     message: 'c = ',
     type: 'number',
     default: 4,
   });
 
-  writeMember(a, 'without plus');
-  write('x^2 ');
+  writeMember(a, true);
+  stdio.write('x^2 ');
   writeMember(b);
-  write('x ');
+  stdio.write('x ');
   writeMember(c);
-  writeln(' = 0');
+  stdio.writeln(' = 0');
 
   const D = Math.pow(b, 2) - 4 * a * c;
-  writeln('D = ' + D);
+  stdio.writeln('D = ' + D);
 
   if (D < 0) {
-    writeln('Решений нет');
+    stdio.writeln('Решений нет');
     return;
   }
 
-  writeln('x1 = ' + (-b + D) / (2 * a));
+  stdio.writeln('x1 = ' + (-b + D) / (2 * a));
 
   if (D > 0) {
-    writeln('x2 = ' + (-b - D) / (2 * a));
+    stdio.writeln('x2 = ' + (-b - D) / (2 * a));
   }
 };
+
+/**
+ * @arg {number} number
+ * @param {true} [isWithoutPlus]
+ */
 
 function writeMember(number, isWithoutPlus) {
   const mod = Math.abs(number);
 
-  if (number >= 0 && !isWithoutPlus) write('+ ');
-  else write('- ');
-  if (mod !== 1) write(mod.toString().bold);
+  if (number >= 0 && !isWithoutPlus) stdio.write('+ ');
+  else stdio.write('- ');
+  if (mod !== 1) stdio.write(mod);
 }
