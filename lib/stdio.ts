7d8cd413385d537a796f4bc35bca708b392fab5b
justerest <justerest@yandex.ru>
2018-01-22 09:18:53 +0500

    style edits

--- lib/stdio.ts
+++ lib/stdio.ts
@@ -2,12 +2,10 @@ import { createInterface } from 'readline';
 import chalk from 'chalk';
 
 /**
-* Input/output interface.
-*
-* _Интерфейс ввода/вывода._
-*
-* @module stdio
-*/
+  * Input/output interface.  
+  * _Интерфейс ввода/вывода._
+  * @module stdio
+  */
 export default {
   readln,
   readlnInt,
@@ -16,39 +14,36 @@ export default {
 };
 
 /**
-* Reads the line entered by user. Returns a string.
-*
-* _Считывает строку, введённую пользователем. Возвращает строку._
-*
-* @memberof stdio
-* @async
-* @param  {string}  message
-* @param  {string}  [defaultValue]  _The value to be assigned in dev mode.
-*                                   Значение, которое будет присваиваться в dev-режиме._
-* @return {Promise<string>}
-* @example
-* const password = await stdio.readln('Введите пароль: ', 'asdasd');
-*/
+  * Reads the line entered by user. Returns a string.
+  * _Считывает строку, введённую пользователем. Возвращает строку._
+  * @memberof stdio
+  * @async
+  * @param  {string}  message
+  * @param  {string}  [defaultValue]  _The value to be assigned in dev mode.
+  *                                   Значение, которое будет присваиваться в dev-режиме._
+  * @return {Promise<string>}
+  * @example
+  * const password = await stdio.readln('Введите пароль: ', 'asdasd');
+  */
 function readln(message: string, defaultValue?: string) {
   return _readln(message, defaultValue);
 }
 
 /**
-* Reads the line entered by user. Returns a number.
-*
-* _Считывает строку, введённую пользователем. Возвращает целое число._
-*
-* @memberof stdio
-* @async
-* @param  {string}  message
-* @param  {number}  [defaultValue]  _The value to be assigned in dev mode.
-*                                   Значение, которое будет присваиваться в dev-режиме._
-* @throws _Will throw an error and exit if can't convert the line into number.
-*         Вызовет ошибку и завершит программу, если не удастся конвертировать строку в число._
-* @return {Promise<number>}
-* @example
-* const yearsOld = await stdio.readlnInt('Сколько Вам лет? ', 15);
-*/
+  * Reads the line entered by user. Returns a number.
+  * _Считывает строку, введённую пользователем. Возвращает целое число._
+  *
+  * @memberof stdio
+  * @async
+  * @param  {string}  message
+  * @param  {number}  [defaultValue]  _The value to be assigned in dev mode.
+  *                                   Значение, которое будет присваиваться в dev-режиме._
+  * @throws _Will throw an error and exit if can't convert the line into number.  
+  *         Вызовет ошибку и завершит программу, если не удастся конвертировать строку в число._
+  * @return {Promise<number>}
+  * @example
+  * const yearsOld = await stdio.readlnInt('Сколько Вам лет? ', 15);
+  */
 async function readlnInt(message: string, defaultValue?: number) {
   const line = await _readln(message, defaultValue);
   const result = parseInt(line, 10);
@@ -60,17 +55,15 @@ async function readlnInt(message: string, defaultValue?: number) {
 }
 
 /**
-* Print a line on the screen. Next output will start from the current line.
-*
-* _Вывод строки на экран. Следующий вывод начнётся с текущей строки._
-*
-* @memberof stdio
-* @param  {string | number} message
-* @example
-* const result = 15;
-* stdio.write('Ответ: ');   // Ответ: 15
-* stdio.writeln(result);
-*/
+  * Print a line on the screen. Next output will start from the current line.  
+  * _Вывод строки на экран. Следующий вывод начнётся с текущей строки._
+  * @memberof stdio
+  * @param  {string | number} message
+  * @example
+  * const result = 15;
+  * stdio.write('Ответ: ');   // Ответ: 15
+  * stdio.writeln(result);
+  */
 function write(message: string | number) {
   const rl = createInterface({
     input: process.stdin,
@@ -81,23 +74,19 @@ function write(message: string | number) {
 }
 
 /**
-* Print a line on the screen. Next output will start from the new line.
-*
-* _Вывод строки на экран. Следующий вывод начнётся с новой строки._
-*
-* @memberof stdio
-* @param  {string | number} message
-* @example
-* stdio.writeln('Вася');  // Вася
-* stdio.writeln(2001);    // 2001
-*/
+  * Print a line on the screen. Next output will start from the new line.  
+  * _Вывод строки на экран. Следующий вывод начнётся с новой строки._
+  * @memberof stdio
+  * @param  {string | number} message
+  * @example
+  * stdio.writeln('Вася');  // Вася
+  * stdio.writeln(2001);    // 2001
+  */
 function writeln(message: string | number) {
   write(message + '\n');
 }
 
-/**
-* @private
-*/
+/** @private */
 async function _readln(message: string, defaultValue?: string | number) {
   const rl = createInterface({
     input: process.stdin,
